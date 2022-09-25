ea7a4ec6fc5ef8c060bfc1bb43b9db8e0daad1ae
Vimal Kumar <Vimal2540.vk@gmail.com>
2022-09-25 21:33:36 +0530

    update readme

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ const qrImage = document.getElementById("img");
 btnSubmit.addEventListener("click", () => {
   if (qrData.value)
     fetch(
-      `http://api.qrserver.com/v1/create-qr-code/?data=${qrData.value}&size=150x150`
+      `https://api.qrserver.com/v1/create-qr-code/?data=${qrData.value}&size=150x150`
     )
       .then((res) => {
         qrImage.style.display = "block";
